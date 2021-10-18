0c081854a7f96e52b3a90bc19071097efe343eea
MiluMihaiFlorin <milu.mihai.y3c@student.ucv.ro>
2021-10-18 09:29:37 +0300

    3. add person

--- 4.6_Manage_a_Car/4.6_Manage_a_Car.cpp
+++ 4.6_Manage_a_Car/4.6_Manage_a_Car.cpp
@@ -3,10 +3,38 @@
 using namespace std;
 //1. set Car license number by constructor, setter or by direct read
 //2. start Car
+//3. add Person (name, address etc) to Car. Person to be added should have a name given by
+//4. set Car’s driver (Person)
+
+//constructor, setter or by direct read
+class Person 
+{
+private:
+    string name;
+    string address;
+public:
+    Person(string name, string address) 
+    {
+        this->name = name;
+        this->address = address;
+    }
+    Person()
+    {
+    }
+    void setName(string name)
+    {
+        this->name = name;
+    }
+    string getName()
+    {
+        return this->name;
+    }
+};
 class Car
 {
 private:
     string licence;
+    Person *person;
 public:
     Car(string licence)
     {
@@ -29,6 +57,15 @@ public:
     {
         cout << "Car started with licence " << this->licence << endl;
     }
+    void setPerson(Person *person)
+    {
+        this->person = person;
+    }
+    Person* getPerson()
+    {
+        return this->person;
+    }
+
 };
 int main()
 {
@@ -42,6 +79,12 @@ int main()
 
     //start car
     car1->startCar();
+
+    //3 add person
+    Person* person1= new Person("Florin" ,"Address1");
+    car1->setPerson(person1);
+
+    cout << car1->getPerson()->getName();
     
     
 }
