b5c6f32f11851bd4099d2c3e46010efca98c7d4c
Emil Geicu <geicu.emil.y3t@student.ucv.ro>
2021-10-18 08:43:03 +0300

    set Car license number by constructor, setter or by direct read

--- 4.6_Manage_a_Car/4.6_Manage_a_Car.cpp
+++ 4.6_Manage_a_Car/4.6_Manage_a_Car.cpp
@@ -1,8 +1,40 @@
 
 #include <iostream>
-
+using namespace std;
+//set Car license number by constructor, setter or by direct read
+class Car
+{
+private:
+    string licence;
+public:
+    Car(string licence)
+    {
+        this->licence = licence;
+    }
+    void setLicence(string licence)
+    {
+        this->licence = licence;
+    }
+    string getLicence()
+    {
+        return this->licence;
+    }
+    void directReadLicence()
+    {
+        cout << "Give a licence number:";
+        cin >> this->licence;
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    Car* car1;
+    car1 = new Car("DJ23RAP");
+    cout << car1->getLicence()<<endl;
+    car1->setLicence("DJ53DCS");
+    cout << car1->getLicence()<<endl;
+    car1->directReadLicence();
+    cout << car1->getLicence() << endl;
+    
+    
 }
 
